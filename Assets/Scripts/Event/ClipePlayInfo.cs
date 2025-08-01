cbb8e8e995d9743bd5e3c7ac5a5d8912107d447b
空格空格感叹号 <469757798@qq.com>
2025-08-01 17:34:10 +0800

    【testScence】

---
+++ Assets/Scripts/Event/ClipePlayInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Space.GlobalInterface.EventInterface;
+using UnityEngine;
+
+public struct ClipePlayInfo : IEventData
+{
+    public int num;
+    //其它参数
+}
